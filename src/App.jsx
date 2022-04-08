64136b7fba22de54a930509ae5c9d38248446952
isabella.pires <pires.isabella@engenharia.ufjf.br>
2022-04-07 21:08:24 -0300

    Novo Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Topbar from "./Components/topbar/Topbar"
+import Intro from "./Components/intro/Intro"
+import Portfolio from "./Components/portfolio/Portfolio"
+import Works from "./Components/works/Works"
+import Testimonials from "./Components/testimonials/Testimonials"
+import Contact from "./Components/contact/Contact"
+import "./app.scss"
+
+function App() {
+  return (
+    <div className="app">
+      <Topbar/>
+      <div className="sections">
+        <Intro/>
+        <Portfolio/>
+        <Works/>
+        <Testimonials/>
+        <Contact/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
